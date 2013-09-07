3853d05a6f55081a5caa5aa05553bb7a839cbc13
Sigit Dewanto <sigitdewanto11@yahoo.co.uk>
2013-09-07 10:23:14 +0700

    Use configuration from server side for sale type.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var SALE_TYPES = ['KULAKAN', 'ECERAN'];
 
 app.use('/static', express.static('public'));
 
 app.get('/sale/new', function(req, res) {
-   res.render('sale/new');
+   res.render('sale/new', {'SALE_TYPES': SALE_TYPES});
 });
 
 app.set('view engine', 'ejs');
